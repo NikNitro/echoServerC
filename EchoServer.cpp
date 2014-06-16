20b20b5367b4274d57ad11896e6965e77b59f6da
NikNitro <adrimlg92@gmail.com>
2014-06-16 21:41:51 +0200

    Quitados bastantes errores. Quedan 4 lineas conflictivas.

--- EchoServer.cpp
+++ EchoServer.cpp
@@ -6,10 +6,15 @@
 //#include <sys\types.h>				// ERROR NO EXISTE
 #include <unistd.h>	
 
+//Otros includes:
+#include<netinet/in.h> 
+#include<arpa/inet.h> 
+#include<netdb.h>
+
 using namespace std;
 
 int listen_socket, cliente;
-struct sockaddr_in clientinfo, servicio;
+struct sockaddr_in clientinfo, servicio; 
 int rtn;
 char buffer[256];
 const int PUERTO = 5050;
@@ -18,25 +23,25 @@ const int PUERTO = 5050;
 int main() {
 
 	//SOCKET
-	listen_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	listen_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);	//‘IPPROTO_TCP’ was not declared in this scope
 
 	servicio.sin_family = PF_INET;
-	servicio.sin_addr.S_un.S_addr = INADDR_ANY;
-	servicio.sin_port = htons(PUERTO);
+	servicio.sin_addr.s_addr = INADDR_ANY;						//‘INADDR_ANY’ was not declared in this scope
+	servicio.sin_port = htons(PUERTO);							//'htons' was not declared in this scope
 	//BIND
 	rtn = bind(listen_socket, (struct sockaddr*) &servicio, (int) sizeof(servicio));
 	//LISTEN
-	listen(listen_socket, SOMAXCONN;
+	listen(listen_socket, SOMAXCONN);
 	//ACCEPT
-	rtn = accept(listen_socket, (struct sockaddr*) &servicio, (int*) sizeof(servicio);
+	rtn = accept(listen_socket, (struct sockaddr*) &servicio, (int*) sizeof(servicio));
 	//READ
-	rtn = read(listen_socket, *buffer, sizeof(buffer))
+	rtn = read(listen_socket, *buffer, sizeof(buffer));
 	while (true) {
 		//WRITE
-		irtn = write(listen_socket, *buffer, sizeof(buffer))
+		rtn = write(listen_socket, *buffer, sizeof(buffer));
 		//READ
-		rtn = read(listen_socket, *buffer, sizeof(buffer))
-		fprintf(stderr, "Bucle");
+		rtn = read(listen_socket, *buffer, sizeof(buffer));
+		cout<<"Bucle";
 	}
 	
 	return 0;
